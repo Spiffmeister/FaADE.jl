95e329544806c3a4974f090145a971fedbab14b4
Spiffmeister <robospiff@gmail.com>
2021-12-20 12:16:52 +1100

    Updated testing, deleted comments in Op_Dx

--- testing/FirstDerivative.jl
+++ testing/FirstDerivative.jl
@@ -16,6 +16,8 @@ function buildgrid(n)
 end
 
 
+
+
 #=
 
 #== 2nd order method ==#
@@ -48,102 +50,127 @@ u = x.^2
 
 =#
 
+#=
 #== 4th order method ==#
 
 # Linear function #1
-
 n, x, Δx = buildgrid(15)
 u = x
 ∂ₓuₑ = ones(n)
 ∂ₓu = Dₓ(u,n,Δx,order=4)
 
+@test norm(∂ₓuₑ[5:n-4] - ∂ₓu[5:n-3]) ≤ 1.0e-14
 
 
 # Quadratic function
-
 n, x, Δx = buildgrid(15)
 u = x.^2
 ∂ₓuₑ = 2x
 ∂ₓu = Dₓ(u,n,Δx,order=4)
 
+@test norm(∂ₓuₑ[5:n-4] - ∂ₓu[5:n-3]) ≤ 1.0e-14
+
 
 
 
 # Cubic
-
-n, x, Δx = buildgrid(20)
+n, x, Δx = buildgrid(15)
 u = x.^3
 ∂ₓuₑ = 3x.^2
 ∂ₓu = Dₓ(u,n,Δx,order=4)
 
+@test norm(∂ₓuₑ[5:n-4] - ∂ₓu[5:n-4]) ≤ 1.0e-14
+
 
 
 # Quartic
-
 n, x, Δx = buildgrid(20)
 u = x.^4
 ∂ₓuₑ = 4x.^3
 ∂ₓu = Dₓ(u,n,Δx,order=4)
 
+@test norm(∂ₓuₑ[5:n-4] - ∂ₓu[5:n-4]) ≤ 1.0e-14
 
 
 
+# Quintic - This test should return the Test Broken expression as it fails to be under the tolerance
+n, x, Δx = buildgrid(15)
+u = x.^5
+∂ₓuₑ = 5x.^4
+∂ₓu = Dₓ(u,n,Δx,order=4)
+
+@test_broken norm(∂ₓuₑ[5:n-4] - ∂ₓu[5:n-4]) ≤ 1.0e-14
+
+=#
 
 
-#=
 
 #== 6th order method ==# 
 
 
 # Linear function #1
 
-n, x, Δx = buildgrid(15)
-
+n, x, Δx = buildgrid(20)
 u = x
-
 ∂ₓuₑ = ones(n)
+∂ₓu = Dₓ(u,n,Δx,order=6)
 
-∂ₓu = Dₓ(u,n,Δx,order=4)
-
-∂ₓuₑ .≈ ∂ₓu
+∂ₓuₑ[7:end-6] .≈ ∂ₓu[7:n-6]
 
 
 # Quadratic function
 
-n, x, Δx = buildgrid(15)
-
+n, x, Δx = buildgrid(20)
 u = x.^2
-
 ∂ₓuₑ = 2x
-
 ∂ₓu = Dₓ(u,n,Δx)
 
-∂ₓuₑ .≈ ∂ₓu
+∂ₓuₑ[7:end-6] .≈ ∂ₓu[7:n-6]
 
 
 # Cubic
 
 n, x, Δx = buildgrid(15)
-
 u = x.^3
-
 ∂ₓuₑ = 3x.^2
-
 ∂ₓu = Dₓ(u,n,Δx)
 
-norm(∂ₓu .- ∂ₓuₑ)
+∂ₓuₑ[7:end-6] .≈ ∂ₓu[7:n-6]
 
 
 # Quartic
 
 n, x, Δx = buildgrid(15)
-
 u = x.^4
-
 ∂ₓuₑ = 4x.^3
-
 ∂ₓu = Dₓ(u,n,Δx)
 
-∂ₓuₑ .≈ ∂ₓu
+∂ₓuₑ[7:end-6] .≈ ∂ₓu[7:n-6]
 
-=#
+
+# Quintic
+n, x, Δx = buildgrid(15)
+u = x.^5
+∂ₓuₑ = 5x.^4
+∂ₓu = Dₓ(u,n,Δx,order=4)
+
+@test norm(∂ₓuₑ[7:n-6] - ∂ₓu[7:n-6]) ≤ 1.0e-14
+
+
+# Quintic - This test should return the Test Broken expression as it fails to be under the tolerance
+n, x, Δx = buildgrid(15)
+u = x.^5
+∂ₓuₑ = 5x.^4
+∂ₓu = Dₓ(u,n,Δx,order=4)
+
+@test_broken norm(∂ₓuₑ[7:n-6] - ∂ₓu[7:n-6]) ≤ 1.0e-14
+
+
+# Quintic - This test should return the Test Broken expression as it fails to be under the tolerance
+n, x, Δx = buildgrid(15)
+u = x.^5
+∂ₓuₑ = 5x.^4
+∂ₓu = Dₓ(u,n,Δx,order=4)
+
+@test_broken norm(∂ₓuₑ[7:n-6] - ∂ₓu[7:n-6]) ≤ 1.0e-14
+
