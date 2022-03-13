3514473703cccc59ed0a1350295356cdb75144bd
spiffmeister <robospiff@gmail.com>
2022-03-13 23:19:20 +1100

    Forgot to push boundary

--- src/BoundaryOp_Dxx.jl
+++ src/BoundaryOp_Dxx.jl
@@ -14,7 +14,7 @@ function SAT_left(type::Symbol,u::Vector{Float64},Δx::Float64,g;order=2,c::Unio
     elseif type == :Robin
         SAT = Robin_left(u,Δx,g,order=order,a=a,b=b)
     else
-        error("Must specify either :Dirichlet, :Neumann or :Robin. For :Periodic use the SAT() function")
+        error("Must specify either :Dirichlet, :Neumann or :Robin. For :Periodic use the Periodic() function")
     end
 
     return SAT
@@ -42,27 +42,6 @@ end
 #=
 ====================== Dirichletee boundary conditions ======================
 =#
-
-function Dirichlet(u::Vector{Float64},Δx::Float64,g::Vector{Float64};c::Union{Float64,Vector{Float64}}=1.0,order::Int64=2,penalty::Vector{Float64}=[-1.0,-1.0])
-    # SAT for second derivative with two sided Dirichlet boundary conditions
-    
-    # If k is a scalar, vectorise it
-    if typeof(c) <: Float64
-        c = zeros(2*order) .+ c
-    end
-
-    # Define the SAT vector for returning
-    SAT = zeros(2*order)
-    
-    # Use sided functions to fill
-    SATₗ = Dirichlet_left(u,Δx,g,c=c)
-    SATᵣ = Dirichlet_right(u,Δx,g,c=c)
-
-    SAT[1:order] = SATₗ[1:order]
-    SAT[order+1:end] = SATᵣ[1:order]
-
-    return SAT
-end
 
 
 function Dirichlet_left(u::Vector{Float64},Δx,g;c=1.0,order=2,penalty=[-1.0,-1.0])
@@ -138,23 +117,6 @@ end
 =#
 
 
-
-function Neumann(u::Vector{Float64},Δx::Float64,g::Vector{Float64};c::Union{Float64,Vector{Float64}}=1.0,order::Int64=2,penalty::Float64=-1.0)
-    # SAT for second derivative with two sided Neumann boundary conditions
-    
-    SAT = zeros(Float64,2*order)
-
-    SATₗ = Neumann_left(u,Δx,g,c=c,order=order)
-    SATᵣ = Neumann_right(u,Δx,g,c=c,order=order)
-
-    # Compute the SATs
-    SAT[1] = SATₗ[1]
-    SAT[end] = SATᵣ[end]
-
-    return SAT
-end
-
-
 function Neumann_left(u::Vector{Float64},Δx::Float64,g;c=1.0,order=2,penalty=1.0)
 
     τ = Neumann_penalties(Δx,order)
@@ -201,17 +163,6 @@ end
 #=
 ====================== Robin boundary conditions ======================
 =#
-
-function Robin(u::Vector{Float64},Δx::Float64,g;c::Union{Float64,Vector{Float64}}=1.0,order::Int64,penalty::Vector{Float64}=[-1.0,-1.0],coeffs=[1.0,1.0])
-    # SAT for second derivative with Robin boundary conditions
-
-    # h required in the SAT terms
-    h = hval(order)
-
-    SAT = zeros(Float64,2*order)
-
-    return SAT
-end
 
 function Robin_left(u::Vector{Float64},Δx::Float64,g;order=2,a=1.0,b=1.0)
 
@@ -270,7 +221,7 @@ function Periodic(u::Vector{Float64},Δx::Float64,c::Vector{Float64};order=2)
     α₀ = 0.5/(h * Δx) # Derivative penatly
     τ₁ = -0.5/(h * Δx) # Symmeteriser penalty
 
-    τ₀ = -max(c[1]/2h,c[end]/2h)/(h * Δx) # Function penalty
+    τ₀ = -max(c[1]/2(h*Δx),c[end]/2(h*Δx))/(h * Δx) # Function penalty
 
 
     SAT₀ = zeros(Float64,2order)
@@ -278,29 +229,94 @@ function Periodic(u::Vector{Float64},Δx::Float64,c::Vector{Float64};order=2)
     SAT₂ = zeros(Float64,2order)
 
 
-    SAT₀[1] = τ₀/(h * Δx) * (u[1] - u[end])
-    SAT₀[end] = τ₀/(h * Δx) * (u[end] - u[1])
+    SAT₀[1] = τ₀ * (u[1] - u[end])
+    SAT₀[end] = τ₀ * (u[end] - u[1])
 
-    L₁u = zeros(Float64,2order) .+ (u[1] - u[end])
+
+    L₁u = zeros(Float64,2order)
+    L₁u[1] = (u[1] - u[end])
+    L₁u[end] = (u[1] - u[end])
     Dᵀu = boundary_Dₓᵀ(L₁u,Δx,order)
 
-    SAT₁[1:order] = τ₁/(h * Δx) * c[1] * Dᵀu[1:order]
-    SAT₁[order+1:end] = -τ₁/(h * Δx) * c[end] * Dᵀu[end-order+1:end] # negative for directional derivative
+    SAT₁[1:order] = τ₁ * c[1] * Dᵀu[1:order]
+    SAT₁[order+1:end] = τ₁ * c[end] * Dᵀu[end-order+1:end] # negative for directional derivative
 
     Du = boundary_Dₓ(u,Δx,order)
-    SAT₂[1] = α₀ * (c[1]*Du[1] + c[end]*Du[end]) # corrected for directional derivative
-    SAT₂[end] = α₀ * (c[1]*Du[1] + c[end]*Du[end]) # corrected directional derivative
+    SAT₂[1] = α₀ * (c[1]*Du[1] - c[end]*Du[end]) # corrected for directional derivative
+    SAT₂[end] = α₀ * (c[1]*Du[1] - c[end]*Du[end]) # corrected directional derivative
 
-    println(Du)
 
-    println(SAT₀)
-    println(SAT₁)
-    println(SAT₂)
+    SAT = SAT₀ + SAT₁ + SAT₂
 
-    SAT = SAT₀ + SAT₁ * 0.0 + SAT₂
-
-    return SAT
+    return SAT[1:order], SAT[order+1:end]
 end
+
+
+
+#=
+====================== Interface boundary ======================
+=#
+
+
+function Split_domain(u⁻,u⁺,Δx⁻,Δx⁺,c⁻,c⁺;order=2,order_left=2,order_right=2)
+
+    h⁻ = hval(order_left)
+    h⁺ = hval(order_right)
+    
+    # Left side
+    α₀ = -0.5
+    τ₁ = 0.5
+    τ₀ = max(c⁻[end]/(2*h⁻*Δx⁻), c⁺[1]/(2*h⁺*Δx⁺))
+
+    SAT₀ = zeros(Float64,2order)
+    SAT₁ = zeros(Float64,2order)
+    SAT₂ = zeros(Float64,2order)
+    
+    # Function condition
+    L₀u = zeros(Float64,2order)
+    L₀u[order] += (u⁻[end] - u⁺[1])
+    L₀u[order+1] += (u⁻[end] - u⁺[1])
+
+    SAT₀[1:order] = τ₀/(h⁻ * Δx⁻) * L₀u[1:order]
+    SAT₀[1:order] = τ₀/(h⁺ * Δx⁺) * L₀u[order+1:end]
+
+    
+    # Symmeteriser
+    DₓᵀL₀u = zeros(Float64,2order)
+
+    DₓᵀL₀u⁻ = boundary_Dₓᵀ(L₀u[1:order],Δx⁻,order)
+    DₓᵀL₀u[1:order] = DₓᵀL₀u⁻[order+1:end]
+    DₓᵀL₀u⁺ = boundary_Dₓᵀ(L₀u[order+1:end],Δx⁺,order)
+    DₓᵀL₀u[order+1:end] = DₓᵀL₀u⁺[1:order]
+
+    SAT₁[1:order] = τ₁/(h⁻ * Δx⁻) * c⁻[end] * DₓᵀL₀u[1:order]
+    SAT₁[order+1:end] = τ₁/(h⁺ * Δx⁺) * c⁺[1] * DₓᵀL₀u[order+1:end]
+
+    # Derivative condition
+    Dₓu⁻ = boundary_Dₓ(u⁻,Δx⁻,order)
+    Dₓu⁺ = boundary_Dₓ(u⁺,Δx⁺,order)
+
+    SAT₂[1:order] = α₀/(h⁻ * Δx⁻) * c⁻[end] * Dₓu⁻[order+1:end]
+    SAT₂[order+1:end] = -α₀/(h⁺ * Δx⁺) * c⁺[1] * Dₓu⁺[1:order]
+
+    # println("Du+: ",Dₓu⁺[1:order])
+    # println("Du+/Deltax: ",c⁺[1] .* Dₓu⁺[1:order] ./ Δx⁺)
+    # println("SAT_0: ",SAT₀[order+1:end])
+    # println("SAT_1: ",SAT₁[order+1:end])
+    # println("SAT_2: ",SAT₂[order+1:end])
+
+    SAT = SAT₀ + SAT₁ + SAT₂
+    
+    SAT⁻ = SAT[1:order]
+    SAT⁺ = SAT[order+1:end]
+
+
+
+    return SAT⁻, SAT⁺
+
+end
+
+
 
 
 #=
@@ -316,33 +332,25 @@ function boundary_Dₓᵀ(u::Vector{Float64},Δx::Float64,order::Int64=2)
 
         BOp = [-1.0, 1.0]
 
-        uₓ[1:2] = BOp*u[1]/Δx
-        
-        # uₓ[1] = -u[1]/Δx
-        # uₓ[2] = u[1]/Δx
-        
-        uₓ[end-1:end] = BOp*u[end]/Δx
-        # uₓ[end-1] = -u[end]/Δx
-        # uₓ[end] = u[end]/Δx
+        uₓ[1:2] = BOp*u[1]/Δx   # Left boundary
+        uₓ[end-1:end] = BOp*u[end]/Δx   # Right boundary
         
     elseif order == 4
         #Boundary for the fourth order case
 
         BOp = [-24.0/17.0, 59.0/34.0, -4.0/17.0, -3.0/34.0]
         
-        uₓ[1:4] = BOp*u[1]/Δx
-        
-        uₓ[end-3:end] = -BOp*u[end]/Δx
+        uₓ[1:4] = BOp*u[1]/Δx   # Left boundary
+        uₓ[end-3:end] = -BOp*u[end]/Δx  # Right boundary
         
         
     elseif order == 6
         #Boundary for the sixth order case
 
         BOp = [-1.582533518939116, 2.033378678700676, -0.141512858744873, -0.450398306578272, 0.104488069284042, 0.036577936277544]
-
-        uₓ[1:6] = BOp*u[1]/Δx
-
-        uₓ[end-5:end] = -BOp*u[end]/Δx
+        
+        uₓ[1:6] = BOp*u[1]/Δx   # Left boundary
+        uₓ[end-5:end] = -BOp*u[end]/Δx  # Right boundary
 
     end
 
