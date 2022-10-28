91b6cf8a70ecf5210f97b5c9f7d3375721d9cdff
spiffmeister <robospiff@gmail.com>
2022-10-28 17:22:44 +1100

    Some updates to the penalty function setup

--- src/Helpers/penaltyfn.jl
+++ src/Helpers/penaltyfn.jl
@@ -4,7 +4,7 @@
 
 
 
-function generate_parallel_penalty(w_f::Function,w_b::Function,order::Int;κ::T=1,τ::T=-1) where T
+function generate_parallel_penalty(w_f::Function,w_b::Function,grid::Grid2D,order::Int;κ::T=1,τ::T=-1,interpmode::Symbol=:linear) where T
 
     H_x = SBP_operators.build_H(ny,order)
     H_y = SBP_operators.build_H(nx,order)
@@ -12,7 +12,13 @@ function generate_parallel_penalty(w_f::Function,w_b::Function,order::Int;κ::T=
     H_x = 1.0 ./H_x.^2
     H_y = 1.0 ./H_y.^2
 
+    let H_x=H_x, H_y=H_y,
+            w_f=w_f, w_b=w_b,
+            grid=grid,
+            τ=τ, κ=κ
 
+        ParPen(u,u₀,Δt) = ParallelPenalty(w_f,w_b,interp,u,u₀,Δt,grid,τ,κ,H_x,H_y)
+    end
 
 
 end
@@ -23,9 +29,9 @@ struct parallel_storage
     B_plane
 end
 
-function P_∥(u::AbstractArray{T},u₀::AbstractArray{T},Δt::T,w_f::Function,w_b::Function,grid::GridType,τ::T,κ::T) where T
-    u_f = w_f()
-    u_b = w_b()
+function ParallelPenalty(w_f::Function,w_b::Function,interp::Function,u::AbstractArray{T},u₀::AbstractArray{T},Δt::T,grid::Grid2D,τ::T,κ::T,H_x::AbstractArray{T},H_y::AbstractArray{T}) where T
+    u_f = w_f(grid)
+    u_b = w_b(grid)
     for j = 1:ny
         for i = 1:nx
             u[i,j] = 1.0/(1.0 - κ/2.0 * Δt * (H_y[i] + H_x[j])) * (u₀[i,j] - Δt*τ/4.0 * (H_y[i]+H_x[j])*(u_f[i,j] + u_b[i,j]))
