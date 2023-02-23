de31ef107a1fbca898b62e2ad87ac1a3bb0799e5
spiffmeister <robospiff@gmail.com>
2023-02-23 18:09:14 +1100

    - Fixed bug with boundary derivative transpose - Removed extra 1/h in penalty terms for Dirichet and Periodic boundaries - Removed - sign on periodic SAT

--- src/SATs/Boundary_Operators.jl
+++ src/SATs/Boundary_Operators.jl
@@ -1,14 +1,14 @@
 """
     BoundaryDerivativeTranspose
 
-Used to construct `EDₓᵀ` where `E=E₀` or `Eₙ`.
+Used to construct `EH⁻¹Dₓᵀ` where `E=E₀` or `Eₙ`.
 """
 function BoundaryDerivativeTranspose(::NodeType{:Left},order::Int,Δx::Float64)
     if order == 2
         return [-1.0,0.5]/Δx
     elseif order == 4
         #[-24.0/17.0 * 48.0/17.0, 59.0/34.0 * 48.0/59.0, -4.0/17.0 * 48.0/43.0, -3.0/34.0 * 48.0/49.0]/Δx
-        return [-1152.0/289.0, 24.0/17.0, -192.0/731.0, -72.0/833.0]/Δx #E₀HDᵀ
+        return [-1152.0/289.0, 24.0/17.0, -192.0/731.0, -72.0/833.0]/Δx #H⁻¹Dᵀ
     elseif order == 6
         return [-1.582533518939116, 2.033378678700676, -0.141512858744873, -0.450398306578272, 0.104488069284042, 0.036577936277544]/Δx
     end
@@ -17,8 +17,8 @@ function BoundaryDerivativeTranspose(::NodeType{:Right},order::Int,Δx::Float64)
     if order == 2
         return [-0.5,1.0]/Δx
     elseif order == 4
-        # return -[-24.0/17.0 * 48.0/49.0, 59.0/34.0 * 48.0/43.0, -4.0/17.0 * 48.0/59.0, -3.0/34.0 * 48.0/17.0]/Δx
-        return [1152.0/833.0, -1416.0/731.0, 192.0/1003.0, 72.0/289.0]/Δx #EₙHDᵀ
+        # return -[-3.0/34.0 * 48.0/49.0, -4.0/17.0 * 48.0/43.0, 59.0/34.0 * 48.0/59.0, -24.0/17.0 * 48.0/17.0]/Δx
+        return [72.0/833.0, 192.0/731.0, -24.0/17.0, 1152.0/289.0]/Δx #H⁻¹Dᵀ
     elseif order == 6
         return [-1.582533518939116, 2.033378678700676, -0.141512858744873, -0.450398306578272, 0.104488069284042, 0.036577936277544]/Δx
     end
@@ -62,7 +62,7 @@ function SATpenalties end
     # For reading in penalty parameters for Dirichlet SATs
     h = hval(order)
 
-    α = 1.0 * (h * Δx)^-1 # α*H^{-1}
+    α = 1.0/Δx # α/Δx -- #1/h accounted for in BoundaryDerivativeTranspose
 
     τ = 1.0
     τ = -(1.0 + τ) * (h * Δx)^-2 # τ*H^{-1}H^{-1}
@@ -85,8 +85,8 @@ end
     h = hval(order)
 
     α₀ = 0.5/(h*Δx)
-    τ₁ = -0.5/(h*Δx)
-    τ₀(c) = -max(c[1],c[end])/(2(h*Δx)^2)
+    τ₁ = -0.5/Δx #1/h accounted for in BoundaryDerivativeTranspose
+    τ₀(c) = -max(c[1],c[end])/(2*(h*Δx)^2)
 
     return α₀,τ₁,τ₀
 end
