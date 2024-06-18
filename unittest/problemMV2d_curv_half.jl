493f26b2f6f15faea92ca6ca0c26a5bb3c4e995f
spiffmeister <robospiff@gmail.com>
2024-06-18 22:22:47 +1000

    Update tests

--- unittest/problemMV2d_curv_half.jl
+++ unittest/problemMV2d_curv_half.jl
@@ -65,38 +65,39 @@ D1 = Grid2D(u->[u*0.5 - 0.25,-0.25],
 
 T = FaADE.Grid.Torus([1.0],[1.0],[1],[0])
 
-# Left domain
-D2 = Grid2D(u->[-0.25,u*0.5 - 0.25],
-            v->v*(T(5π/4,0.0) - [-0.25, -0.25]) + [-0.25, -0.25],
-            v->v*(T(3π/4,0.0) - [-0.25,0.25]) + [-0.25,0.25],
-            u->T(u*(3π/4 - 5π/4) + 5π/4,0.0),
+# Right domain
+D2 = Grid2D(u->[0.25, -u*0.5 + 0.25], # Bottom
+            v->v*(T(π/4,0.0) - [0.25,0.25]) + [0.25,0.25], # Left
+            v->v*(T(7π/4,0.0) + [-0.25, 0.25]) + [0.25, -0.25], # Right
+            u->T(u*(7π/4 - 9π/4) + 9π/4,0.0), # Top
             nx,ny)
-joints = ((Joint(2,Left),),
-            (Joint(1,Down),))
-
 
 # Bottom domain
-# D2 = Grid2D(u->[-u*0.5 + 0.25, -0.25],
-#             v->v*(T(7π/4,0.0) - [0.25,-0.25]) + [0.25, -0.25],
-#             v->v*(T(5π/4,0.0) - [-0.25,-0.25]) + [-0.25, -0.25],
-#             u->T(u*(5π/4 - 7π/4) + 7π/4, 0.0),
-#             nx,ny)
-# joints = ((Joint(2,Down),),
-#             (Joint(1,Down),))
+D3 = Grid2D(u->[-u*0.5 + 0.25, -0.25],
+            v->v*(T(7π/4,0.0) - [0.25,-0.25]) + [0.25, -0.25],
+            v->v*(T(5π/4,0.0) - [-0.25,-0.25]) + [-0.25, -0.25],
+            u->T(u*(5π/4 - 7π/4) + 7π/4, 0.0),
+            nx,ny)
 
 
-Dom = GridMultiBlock((D1,D2),joints)
+joints = ((Joint(2,Right),Joint(3,Down)),
+            (Joint(1,Down),Joint(3,Right)),
+            (Joint(1,Down),Joint(2,Left)))
 
-# Dl1 = FaADE.SATs.SAT_Dirichlet(Bxy, D1.Δx, Left, order, D1.Δy, :Curvilinear) # Block 1 BCs
-Dr1 = FaADE.SATs.SAT_Dirichlet(Bxy, D1.Δx, Right, order, D1.Δy, :Curvilinear) # Block 2 BCs
-Du1 = FaADE.SATs.SAT_Dirichlet(Bxy, D1.Δy, Up, order, D1.Δx, :Curvilinear) # Block 3 BCs
-Dd1 = FaADE.SATs.SAT_Dirichlet(Bxy, D1.Δy, Down, order, D1.Δx, :Curvilinear) # Block 3 BCs
 
-Dd2 = FaADE.SATs.SAT_Dirichlet(Bxy, D2.Δy, Up, order, D2.Δx, :Curvilinear) # Block 4 BCs - local up
-Dl2 = FaADE.SATs.SAT_Dirichlet(Bxy, D2.Δx, Left, order, D2.Δy, :Curvilinear) # Block 5 BCs
-Dr2 = FaADE.SATs.SAT_Dirichlet(Bxy, D2.Δx, Right, order, D2.Δy, :Curvilinear) # Block 1 BCs
+Dom = GridMultiBlock((D1,D2,D3),joints)
 
-BD = Dict(1 => (Dd1,Dr1,Du1), 2 => (Dd2,Dr2,Dl2))
+Dl1 = SAT_Dirichlet(Bxy, D1.Δx, Left,   order, D1.Δy, :Curvilinear) # Block 1 BCs
+Du1 = SAT_Dirichlet(Bxy, D1.Δy, Up,     order, D1.Δx, :Curvilinear) # Block 1 BCs
+
+Dl2 = SAT_Dirichlet(Bxy, D2.Δx, Left,order, D2.Δy, :Curvilinear) # Block 2 BCs
+Du2 = SAT_Dirichlet(Bxy, D2.Δy, Up,  order, D2.Δx, :Curvilinear) # Block 2 BCs
+
+Dr3 = SAT_Dirichlet(Bxy, D3.Δx, Right,  order, D3.Δy, :Curvilinear) # Block 4 BCs
+Du3 = SAT_Dirichlet(Bxy, D3.Δy, Up,     order, D3.Δx, :Curvilinear) # Block 4 BCs
+
+
+BD = Dict(1 => (Dl1,Du1), 2 => (Dl2,Du2), 3 => (Dr3,Du3))
 
 
 
@@ -105,6 +106,7 @@ gridfig = Figure()
 gridfix_ax = Axis(gridfig[1,1])
 scatter!(gridfix_ax,D1.gridx[:],D1.gridy[:],markersize=10.5)
 scatter!(gridfix_ax,D2.gridx[:],D2.gridy[:],markersize=10.5)
+scatter!(gridfix_ax,D3.gridx[:],D3.gridy[:],markersize=10.5)
 gridfig
 
 
@@ -118,13 +120,10 @@ soln = solve(P,Dom,Δt,t)
 colourrange = (minimum(minimum.(soln.u[2])),maximum(maximum.(soln.u[2])))
 
 f = Figure()
-ax2 = Axis3(f[1,2])
-surface!(ax2,Dom.Grids[1].gridx, Dom.Grids[1].gridy, soln.u[2][1],colorbar=false)#, colorrange=colourrange)
-surface!(ax2,Dom.Grids[2].gridx, Dom.Grids[2].gridy, soln.u[2][2],colorbar=false)#, colorrange=colourrange)
-
-ax1 = Axis3(f[1,1])
-surface!(ax1,Dom.Grids[1].gridx, Dom.Grids[1].gridy, soln.u[1][1],colorbar=false)#, colorrange=colourrange)
-surface!(ax1,Dom.Grids[2].gridx, Dom.Grids[2].gridy, soln.u[1][2],colorbar=false)#, colorrange=colourrange)
+ax = Axis3(f[1,1])
+surface!(ax,Dom.Grids[1].gridx, Dom.Grids[1].gridy, soln.u[2][1],colorbar=false, colorrange=colourrange)
+surface!(ax,Dom.Grids[2].gridx, Dom.Grids[2].gridy, soln.u[2][2],colorbar=false, colorrange=colourrange)
+surface!(ax,Dom.Grids[3].gridx, Dom.Grids[3].gridy, soln.u[2][3],colorbar=false, colorrange=colourrange)
 
 # scatter!(ax,D1.gridx[:],D1.gridy[:],-ones(length(D1)),markersize=1.5)
 # scatter!(ax,D2.gridx[:],D2.gridy[:],-ones(length(D2)),markersize=1.5)
